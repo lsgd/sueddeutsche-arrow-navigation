39ea03bb29256c3af131740fe45b059cf71af9f7
Lukas Schulze <gitlab@mail.lukas-schulze.de>
2016-11-05 20:17:20 +0100

    initial commit

---
+++ js/sueddeutsche.js
@@ -0,0 +1,40 @@
+var sdz = {};
+
+$(function() {
+    sdz.init();
+});
+
+sdz.init = function() {
+    var $article = $("article.gallery:first");
+    if($article.length < 1) {
+        return;
+    }
+
+    sdz.initNavigation($article.data("bind"));
+    $(".iqadgalleryshim, .iqadgalleryoverlay").remove();
+};
+
+sdz.initNavigation = function($json) {
+    $(document).keyup(function(e) {
+        switch(e.which) {
+            case 37: // left
+                // first image -> cannot go back
+                if ($json.imageGallery.prevURL == $json.imageGallery.baseURL + "-" + $json.imageGallery.total) {
+                    return;
+                }
+                window.location.href = $json.imageGallery.prevURL;
+                break;
+
+            case 39: // right
+                // last image -> cannot go further
+                if ($json.imageGallery.nextURL == $json.imageGallery.baseURL + "-" + $json.imageGallery.total) {
+                    return;
+                }
+                window.location.href = $json.imageGallery.nextURL;
+                break;
+
+            default: return; // exit this handler for other keys
+        }
+        e.preventDefault(); // prevent the default action (scroll / move caret)
+    });
+};
