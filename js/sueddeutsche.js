ae0c3eddbd842c7f985c52a58036fd0f98f17387
Lukas Schulze <gitlab@mail.lukas-schulze.de>
2016-11-05 23:38:17 +0100

    scroll down to images of gallery

--- js/sueddeutsche.js
+++ js/sueddeutsche.js
@@ -22,7 +22,7 @@ sdz.initNavigation = function($json) {
                 if ($json.imageGallery.prevURL == $json.imageGallery.baseURL + "-" + $json.imageGallery.total) {
                     return;
                 }
-                window.location.href = $json.imageGallery.prevURL;
+                window.location.href = $json.imageGallery.prevURL + "#wrapper";
                 break;
 
             case 39: // right
@@ -30,7 +30,7 @@ sdz.initNavigation = function($json) {
                 if ($json.imageGallery.nextURL == $json.imageGallery.baseURL + "-" + $json.imageGallery.total) {
                     return;
                 }
-                window.location.href = $json.imageGallery.nextURL;
+                window.location.href = $json.imageGallery.nextURL + "#wrapper";
                 break;
 
             default: return; // exit this handler for other keys
